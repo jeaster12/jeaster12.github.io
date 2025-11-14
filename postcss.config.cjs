1e6abb212174bfc8e091e54a1cfa9ddd9c4e403d
jeaster12 <jeaster12@gmail.com>
2025-11-13 21:40:16 -0500

    cleanup

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,9 +1,6 @@
 module.exports = {
-  from: undefined, // This will be set by the bundler
-  to: undefined,   // This will be set by the bundler
-  map: process.env.NODE_ENV === 'production' ? false : { inline: true },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
-const colors = require('tailwindcss/colors');
-
+let colors = require('tailwindcss/colors');
+delete colors.lightBlue;
+delete colors.warmGray;
+delete colors.trueGray;
+delete colors.coolGray;
+delete colors.blueGray;
 
 /** @type {import('tailwindcss').Config} */
 export default {
